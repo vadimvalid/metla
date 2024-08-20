aaad121a853507df8aed0dac3ad7d04ff381c1df
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-08-20 15:11:32 +0300

    Add how we clean section

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -2,8 +2,6 @@ const tabs = () => {
   const tabs = document.querySelector('.tab-wrap');
   const tabButton = document.querySelectorAll('.tab');
   const contents = document.querySelectorAll('.tab__content');
-  const moreButton = document.querySelectorAll('.tab__content__more');
-  const hiddenContent = document.querySelectorAll('.tab__content .hidden');
 
   tabs.addEventListener('click', (e) => {
     const id = e.target.dataset.id;
@@ -19,16 +17,6 @@ const tabs = () => {
       const element = document.getElementById(id);
       element.classList.add('active');
     }
-  });
-
-  moreButton.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      btn.textContent = 'Roll up';
-      btn.classList.toggle('active');
-      hiddenContent.forEach((content) => {
-        content.classList.toggle('hidden');
-      });
-    });
   });
 };
 
