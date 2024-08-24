8094b3299fa7aa7ee80757ed455375d1c790b942
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-08-24 11:50:40 +0300

    Add order hero section

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,35 @@
 // form inputs button decrement and increment
-function inputCounter(block) {
+const inputCounter = (block) => {
   const parent = document.querySelector(block);
-  const btnDec = parent.querySelector('[data-action="decrement"]');
-  const btnInc = parent.querySelector('[data-action="increment"]');
-  const val = parent.querySelector('[data-counter="value"]');
 
-  let counter = 1;
-  btnDec.addEventListener('click', () => {
-    if (counter > 1) {
-      counter--;
-      val.value = counter;
-    }
-    if (val.value < val.attributes.max.value) {
-      btnInc.disabled = false;
-    }
-  });
-  btnInc.addEventListener('click', () => {
-    if (val.value < val.attributes.max.value) {
-      counter++;
-      val.value = counter;
-    } else {
-      // btn disabled
-      btnInc.disabled = true;
-    }
-  });
-}
+  if (parent) {
+    const btnDec = parent.querySelector('[data-action="decrement"]');
+    const btnInc = parent.querySelector('[data-action="increment"]');
+    const val = parent.querySelector('[data-counter="value"]');
+
+    let counter = 1;
+    btnDec.addEventListener('click', () => {
+      if (counter > 1) {
+        counter--;
+        val.value = counter;
+      }
+      if (val.value < val.attributes.max.value) {
+        btnInc.disabled = false;
+      }
+    });
+    btnInc.addEventListener('click', () => {
+      if (val.value < val.attributes.max.value) {
+        counter++;
+        val.value = counter;
+      } else {
+        // btn disabled
+        btnInc.disabled = true;
+      }
+    });
+  }
+
+  return false;
+};
 
 inputCounter('.form-item--rooms');
 inputCounter('.form-item--sanuzel');
@@ -32,18 +37,46 @@ inputCounter('.form-item--sanuzel');
 // form square block
 const selectActiveButton = (block) => {
   const parent = document.querySelector(block);
-  const button = parent.querySelectorAll('button');
 
-  button.forEach((element) => {
-    element.addEventListener('click', () => {
-      element.classList.add('active');
-      button.forEach((el) => {
-        if (el !== element) {
-          el.classList.remove('active');
-        }
+  if (parent) {
+    const button = parent.querySelectorAll('button');
+
+    button.forEach((element) => {
+      element.addEventListener('click', () => {
+        element.classList.add('active');
+        button.forEach((el) => {
+          if (el !== element) {
+            el.classList.remove('active');
+          }
+        });
       });
     });
-  });
+  }
+
+  return false;
+};
+
+// select types item
+const selectTypesItem = (block) => {
+  const parent = document.querySelector(block);
+
+  if (parent) {
+    const items = parent.querySelectorAll('.types__item');
+
+    items.forEach((element) => {
+      element.addEventListener('click', () => {
+        element.classList.add('active');
+        items.forEach((el) => {
+          if (el !== element) {
+            el.classList.remove('active');
+          }
+        });
+      });
+    });
+  }
+
+  return false;
 };
 
 selectActiveButton('.form-item__square');
+selectTypesItem('.types .wrapper');
