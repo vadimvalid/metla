9062e8109c9dff3245cbbbd141ece054883a640e
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-08-25 12:13:37 +0300

    Add contacts section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -15,38 +15,10 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-    },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-  },
-});
-
-//  swiper dry
-const swiperDry = new Swiper('.swiper--dry', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+  slidesPerView: 'auto',
   breakpoints: {
     // when window width is >= 768px
     768: {
-      slidesPerView: 5,
       spaceBetween: 20,
     },
   },
