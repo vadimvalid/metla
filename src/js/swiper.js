b108c2ab4b947cfd060f0210946dac9f88017cae
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-08-27 21:52:41 +0300

    Add reviews page

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -16,10 +16,22 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
   slidesPerView: 'auto',
-  breakpoints: {
-    // when window width is >= 768px
-    768: {
-      spaceBetween: 20,
-    },
+  spaceBetween: 20,
+});
+
+const swiperReviews = new Swiper('.swiper-reviews', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 'auto',
+  spaceBetween: 20,
 });
