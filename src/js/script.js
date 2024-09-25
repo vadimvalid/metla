75af92c15fb8ce99efc3325c0b3fca806174ba63
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-09-25 21:18:34 +0300

    Add drag for cards

--- src/js/script.js
+++ src/js/script.js
@@ -447,3 +447,15 @@ const openCleanerOrders = () => {
   }
 };
 openCleanerOrders();
+
+const pointerScroll = (elem) => {
+  const dragStart = (ev) => elem.setPointerCapture(ev.pointerId);
+  const dragEnd = (ev) => elem.releasePointerCapture(ev.pointerId);
+  const drag = (ev) =>
+    elem.hasPointerCapture(ev.pointerId) && (elem.scrollLeft -= ev.movementX);
+
+  elem.addEventListener('pointerdown', dragStart);
+  elem.addEventListener('pointerup', dragEnd);
+  elem.addEventListener('pointermove', drag);
+};
+document.querySelectorAll('.services .wrapper').forEach(pointerScroll);
